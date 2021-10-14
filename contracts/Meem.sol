2e63caeab94deca5c2da45a388f9b3c9bae87105
Ken Goldfarb <hello@kengoldfarb.com>
2021-10-14 15:36:24 -0400

    refactor to use structs

--- contracts/Meem.sol
+++ contracts/Meem.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+pragma experimental ABIEncoderV2;
 
 import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol';
@@ -28,8 +29,6 @@ contract Meem is
 	ERC721URIStorageUpgradeable,
 	MeemStandard
 {
-	event InviterSet(uint256 tokenId, address inviter);
-
 	using CountersUpgradeable for CountersUpgradeable.Counter;
 	using StringsUpgradeable for uint256;
 
@@ -39,31 +38,10 @@ contract Meem is
 
 	CountersUpgradeable.Counter private _tokenIdCounter;
 
-	// Mapping from token ID to inviter address
-	mapping(uint256 => address) private _inviters;
 	address private _tokenURIContractAddress;
 	string private _contractURI;
-
-	mapping(uint256 => Permission) private _copyPermission;
-	mapping(uint256 => Permission) private _remixPermission;
-	mapping(uint256 => Permission) private _readPermission;
-	mapping(uint256 => address[]) private _copyPermissionAddresses;
-	mapping(uint256 => address[]) private _remixPermissionAddresses;
-	mapping(uint256 => address[]) private _readPermissionAddresses;
-	mapping(uint256 => uint256[]) private _copyPermissionNumTokens;
-	mapping(uint256 => uint256[]) private _remixPermissionNumTokens;
-	mapping(uint256 => uint256[]) private _readPermissionNumTokens;
-	mapping(uint256 => address) private _copyLockedBy;
-	mapping(uint256 => address) private _remixLockedBy;
-	mapping(uint256 => address) private _readLockedBy;
-	mapping(uint256 => Chain) private _chain;
-	mapping(uint256 => address) private _parent;
-	mapping(uint256 => uint256) private _parentTokenId;
-	mapping(uint256 => address[]) private _splitAddresses;
-	mapping(uint256 => uint256[]) private _splitAmounts;
-	mapping(uint256 => address[]) private _splitLockedBy;
+	mapping(uint256 => MeemProperties) private _properties;
 	mapping(uint256 => string) private _tokenURIs;
-	mapping(uint256 => uint256) private _totalCopies;
 
 	function initialize(address _proxyRegistryAddress) public initializer {
 		__ERC721Tradable_init('Meem', 'MEEM', _proxyRegistryAddress);
@@ -129,54 +107,45 @@ contract Meem is
 		return super.supportsInterface(interfaceId);
 	}
 
-	// function mint(address to) public onlyRole(MINTER_ROLE) {
-	// 	_safeMint(to, _tokenIdCounter.current());
-	// 	_inviters[_tokenIdCounter.current()] = msg.sender;
-	// 	emit InviterSet(_tokenIdCounter.current(), msg.sender);
-	// 	_tokenIdCounter.increment();
-	// }
-
 	function mint(
+		address to,
 		string memory mTokenURI,
-		address mTo,
-		Chain mChain,
-		address mParent,
-		uint256 mParentTokenId,
-		Permission[] memory mCopyRemixReadPermission,
-		address[] memory mCopyPermissionAddresses,
-		address[] memory mRemixPermissionAddresses,
-		address[] memory mReadPermissionAddresses,
-		address[] memory mCopyRemixReadLockedByAddress,
-		address[] memory mSplitAddresses,
-		uint256[] memory mSplitAmounts
+		MeemProperties memory mProperties
 	) public override onlyRole(MINTER_ROLE) {
 		uint256 tokenId = _tokenIdCounter.current();
-		_safeMint(mTo, tokenId);
+		_safeMint(to, tokenId);
 		_tokenURIs[tokenId] = mTokenURI;
-		_chain[tokenId] = mChain;
-		_parent[tokenId] = mParent;
-		_parentTokenId[tokenId] = mParentTokenId;
-		require(
-			mCopyRemixReadPermission.length == 3,
-			'All permissions must be set.'
-		);
-		_copyPermission[tokenId] = mCopyRemixReadPermission[0];
-		_remixPermission[tokenId] = mCopyRemixReadPermission[1];
-		_readPermission[tokenId] = mCopyRemixReadPermission[2];
-		_copyPermissionAddresses[tokenId] = mCopyPermissionAddresses;
-		_remixPermissionAddresses[tokenId] = mRemixPermissionAddresses;
-		_readPermissionAddresses[tokenId] = mReadPermissionAddresses;
-		require(
-			mCopyRemixReadLockedByAddress.length == 3,
-			"All permissions 'locked by' must be set"
-		);
-		_copyLockedBy[tokenId] = mCopyRemixReadLockedByAddress[0];
-		_remixLockedBy[tokenId] = mCopyRemixReadLockedByAddress[1];
-		_readLockedBy[tokenId] = mCopyRemixReadLockedByAddress[2];
-		// _splitAddresses[tokenId] = mSplitAddresses;
-		// _splitAmounts[tokenId] = mSplitAmounts;
-		// _totalCopies[tokenId] = mTotalCopies;
-		setSplits(tokenId, mSplitAddresses, mSplitAmounts);
+		validateSplits(tokenId, mProperties.splits);
+
+		// Initializes mapping w/ default values
+		delete _properties[tokenId];
+
+		for (uint256 i = 0; i < mProperties.copyPermissions.length; i++) {
+			_properties[tokenId].copyPermissions.push(
+				mProperties.copyPermissions[i]
+			);
+		}
+
+		for (uint256 i = 0; i < mProperties.remixPermissions.length; i++) {
+			_properties[tokenId].remixPermissions.push(
+				mProperties.remixPermissions[i]
+			);
+		}
+
+		for (uint256 i = 0; i < mProperties.readPermissions.length; i++) {
+			_properties[tokenId].readPermissions.push(
+				mProperties.readPermissions[i]
+			);
+		}
+
+		for (uint256 i = 0; i < mProperties.splits.length; i++) {
+			_properties[tokenId].splits.push(mProperties.splits[i]);
+		}
+
+		_properties[tokenId].chain = mProperties.chain;
+		_properties[tokenId].parent = mProperties.parent;
+		_properties[tokenId].parentTokenId = mProperties.parentTokenId;
+		_properties[tokenId].totalCopies = mProperties.totalCopies;
 		_tokenIdCounter.increment();
 	}
 
@@ -210,141 +179,12 @@ contract Meem is
 		_setProxyRegistryAddress(addr);
 	}
 
-	/** The total number of copies allowed for a Meem */
-	// function totalCopies(uint256 tokenId) external view returns (uint256);
-
-	// function setTotalCopies(uint256 numCopies) external;
-
-	/** The address of the parent contract NFT */
-	function parent(uint256 tokenId) public view override returns (address) {
-		return _parent[tokenId];
-	}
-
-	/** The tokenId of the parent contract NFT */
-	function parentTokenId(uint256 tokenId)
+	function properties(uint256 tokenId)
 		public
 		view
-		override
-		returns (uint256)
+		returns (MeemProperties memory)
 	{
-		return _parentTokenId[tokenId];
-	}
-
-	/** The chain of the parent contract NFT  */
-	function parentChain(uint256 tokenId) public view override returns (Chain) {
-		return _chain[tokenId];
-	}
-
-	function copyPermission(uint256 tokenId)
-		public
-		view
-		override
-		returns (Permission)
-	{
-		return _copyPermission[tokenId];
-	}
-
-	function remixPermission(uint256 tokenId)
-		public
-		view
-		override
-		returns (Permission)
-	{
-		return _remixPermission[tokenId];
-	}
-
-	function readPermission(uint256 tokenId)
-		public
-		view
-		override
-		returns (Permission)
-	{
-		return _readPermission[tokenId];
-	}
-
-	function copyPermissionAddresses(uint256 tokenId)
-		public
-		view
-		override
-		returns (address[] memory)
-	{
-		return _copyPermissionAddresses[tokenId];
-	}
-
-	function remixPermissionAddresses(uint256 tokenId)
-		public
-		view
-		override
-		returns (address[] memory)
-	{
-		return _remixPermissionAddresses[tokenId];
-	}
-
-	function readPermissionAddresses(uint256 tokenId)
-		public
-		view
-		override
-		returns (address[] memory)
-	{
-		return _readPermissionAddresses[tokenId];
-	}
-
-	function copyPermissionNumTokens(uint256 tokenId)
-		public
-		view
-		override
-		returns (uint256[] memory)
-	{
-		return _copyPermissionNumTokens[tokenId];
-	}
-
-	function remixPermissionNumTokens(uint256 tokenId)
-		public
-		view
-		override
-		returns (uint256[] memory)
-	{
-		return _remixPermissionNumTokens[tokenId];
-	}
-
-	function readPermissionNumTokens(uint256 tokenId)
-		public
-		view
-		override
-		returns (uint256[] memory)
-	{
-		return _readPermissionNumTokens[tokenId];
-	}
-
-	function numSplits(uint256 tokenId) public view override returns (uint256) {
-		return _splitAddresses[tokenId].length;
-	}
-
-	function splitAddresses(uint256 tokenId)
-		public
-		view
-		override
-		returns (address[] memory)
-	{
-		return _splitAddresses[tokenId];
-	}
-
-	function splitAmounts(uint256 tokenId)
-		public
-		view
-		override
-		returns (uint256[] memory)
-	{
-		return _splitAmounts[tokenId];
-	}
-
-	function splitLockedBy(uint256 tokenId)
-		public
-		view
-		override
-		returns (address[] memory)
-	{
-		return _splitLockedBy[tokenId];
+		return _properties[tokenId];
 	}
 
 	// Internal functions
@@ -383,11 +223,6 @@ contract Meem is
 		uint256 tokenId
 	) internal virtual override {
 		super._transfer(from, to, tokenId);
-
-		// Since the token was transferred, the inviter of the token becomes the from address
-		_inviters[tokenId] = from;
-
-		emit InviterSet(tokenId, from);
 	}
 
 	function contractURI() public view returns (string memory) {
@@ -401,22 +236,17 @@ contract Meem is
 	}
 
 	// Private functions
-	function setSplits(
-		uint256 tokenId,
-		address[] memory mSplitAddresses,
-		uint256[] memory mSplitAmounts
-	) private {
-		require(
-			mSplitAddresses.length == mSplitAmounts.length,
-			'Split addresses and amounts length must match'
-		);
+	function validateSplits(uint256 tokenId, Split[] memory splits)
+		private
+		view
+	{
 		uint256 totalAmount = 0;
 		address tokenOwner = ownerOf(tokenId);
 		// Require that split amounts
-		for (uint256 i = 0; i < mSplitAddresses.length; i++) {
-			totalAmount += mSplitAmounts[i];
+		for (uint256 i = 0; i < splits.length; i++) {
+			totalAmount += splits[i].amount;
 			require(
-				mSplitAddresses[i] != tokenOwner,
+				splits[i].toAddress != tokenOwner,
 				'Splits can not be assigned to token owner'
 			);
 		}
@@ -425,8 +255,5 @@ contract Meem is
 			totalAmount <= 10000,
 			'Total basis points amount must be less than 10000 (100%)'
 		);
-
-		_splitAddresses[tokenId] = mSplitAddresses;
-		_splitAmounts[tokenId] = mSplitAmounts;
 	}
 }
--- contracts/MeemStandard.sol
+++ contracts/MeemStandard.sol
@@ -15,88 +15,31 @@ interface MeemStandard {
 		Holders
 	}
 
-	/** The total number of copies allowed for a Meem */
-	// function totalCopies(uint256 tokenId) external view returns (uint256);
+	struct Split {
+		address toAddress;
+		uint256 amount;
+	}
+	struct MeemPermission {
+		Permission permission;
+		address[] addresses;
+		uint256 numTokens;
+		address lockedBy;
+	}
 
-	// function setTotalCopies(uint256 numCopies) external;
-
-	/** The address of the parent contract NFT */
-	function parent(uint256 tokenId) external view returns (address);
-
-	/** The tokenId of the parent contract NFT */
-	function parentTokenId(uint256 tokenId) external view returns (uint256);
-
-	/** The chain of the parent contract NFT  */
-	function parentChain(uint256 tokenId) external view returns (Chain);
-
-	function copyPermission(uint256 tokenId) external view returns (Permission);
-
-	function remixPermission(uint256 tokenId)
-		external
-		view
-		returns (Permission);
-
-	function readPermission(uint256 tokenId) external view returns (Permission);
-
-	function copyPermissionAddresses(uint256 tokenId)
-		external
-		view
-		returns (address[] memory);
-
-	function remixPermissionAddresses(uint256 tokenId)
-		external
-		view
-		returns (address[] memory);
-
-	function readPermissionAddresses(uint256 tokenId)
-		external
-		view
-		returns (address[] memory);
-
-	function copyPermissionNumTokens(uint256 tokenId)
-		external
-		view
-		returns (uint256[] memory);
-
-	function remixPermissionNumTokens(uint256 tokenId)
-		external
-		view
-		returns (uint256[] memory);
-
-	function readPermissionNumTokens(uint256 tokenId)
-		external
-		view
-		returns (uint256[] memory);
-
-	function numSplits(uint256 tokenId) external view returns (uint256);
-
-	function splitAddresses(uint256 tokenId)
-		external
-		view
-		returns (address[] memory);
-
-	function splitAmounts(uint256 tokenId)
-		external
-		view
-		returns (uint256[] memory);
-
-	function splitLockedBy(uint256 tokenId)
-		external
-		view
-		returns (address[] memory);
+	struct MeemProperties {
+		MeemPermission[] copyPermissions;
+		MeemPermission[] remixPermissions;
+		MeemPermission[] readPermissions;
+		Chain chain;
+		address parent;
+		uint256 parentTokenId;
+		Split[] splits;
+		uint256 totalCopies;
+	}
 
 	function mint(
+		address to,
 		string memory mTokenURI,
-		address mTo,
-		Chain mChain,
-		address mParent,
-		uint256 mParentTokenId,
-		Permission[] memory mCopyRemixReadPermission,
-		address[] memory mCopyPermissionAddresses,
-		address[] memory mRemixPermissionAddresses,
-		address[] memory mReadPermissionAddresses,
-		address[] memory mCopyRemixReadLockedByAddress,
-		address[] memory mSplitAddresses,
-		uint256[] memory mSplitAmounts
+		MeemProperties memory properties
 	) external;
 }
